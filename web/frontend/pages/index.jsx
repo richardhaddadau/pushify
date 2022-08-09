52e74ca2ed6428ea1f1fcacaee8e77c558447de7
Richard Haddad <richard@haddads.net.au>
2022-08-10 06:05:07 +1000

    build: home page

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,86 +1,71 @@
+import { useNavigate, TitleBar, Loading } from "@shopify/app-bridge-react";
 import {
-  Card,
-  Page,
-  Layout,
-  TextContainer,
-  Image,
-  Stack,
-  Link,
-  Heading,
+    Card,
+    EmptyState,
+    Layout,
+    Page,
+    TextContainer,
+    Image,
+    Stack,
+    Link,
+    Heading,
+    SkeletonBodyText,
 } from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
 
 import { trophyImage } from "../assets";
 
 import { ProductsCard } from "../components";
 
 export default function HomePage() {
-  return (
-    <Page narrowWidth>
-      <TitleBar title="App name" primaryAction={null} />
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <Stack
-              wrap={false}
-              spacing="extraTight"
-              distribution="trailing"
-              alignment="center"
-            >
-              <Stack.Item fill>
-                <TextContainer spacing="loose">
-                  <Heading>Nice work on building a Shopify app 🎉</Heading>
-                  <p>
-                    Your app is ready to explore! It contains everything you
-                    need to get started including the{" "}
-                    <Link url="https://polaris.shopify.com/" external>
-                      Polaris design system
-                    </Link>
-                    ,{" "}
-                    <Link url="https://shopify.dev/api/admin-graphql" external>
-                      Shopify Admin API
-                    </Link>
-                    , and{" "}
-                    <Link
-                      url="https://shopify.dev/apps/tools/app-bridge"
-                      external
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    UI library and components.
-                  </p>
-                  <p>
-                    Ready to go? Start populating your app with some sample
-                    products to view and test in your store.{" "}
-                  </p>
-                  <p>
-                    Learn more about building out your app in{" "}
-                    <Link
-                      url="https://shopify.dev/apps/getting-started/add-functionality"
-                      external
-                    >
-                      this Shopify tutorial
-                    </Link>{" "}
-                    📚{" "}
-                  </p>
-                </TextContainer>
-              </Stack.Item>
-              <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
-                  <Image
-                    source={trophyImage}
-                    alt="Nice work on building a Shopify app"
-                    width={120}
-                  />
-                </div>
-              </Stack.Item>
-            </Stack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section>
-          <ProductsCard />
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
+    // Navigation Hook
+    const navigate = useNavigate();
+
+    // Mock Values
+    const isLoading = false;
+    const isReloading = false;
+    const syncJobs = [];
+
+    const loadingMarkup = isLoading ? (
+        <Card sectioned>
+            <Loading />
+            <SkeletonBodyText />
+        </Card>
+    ) : null;
+
+    const emptyStateMarkup =
+        !isLoading && !syncJobs?.length ? (
+            <Card sectioned>
+                <EmptyState
+                    heading="Set up new syncing jobs"
+                    action={{
+                        content: "Create new job",
+                        onAction: () => navigate("/jobs/new"),
+                    }}
+                    image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+                >
+                    <p>
+                        Save syncing jobs with preset settings for quicker
+                        processing.
+                    </p>
+                </EmptyState>
+            </Card>
+        ) : null;
+
+    return (
+        <Page narrowWidth>
+            <TitleBar
+                title="Pushify"
+                primaryAction={{
+                    content: "Create new job",
+                    onAction: () => navigate("/jobs/new"),
+                }}
+            />
+            <Layout>
+                <Layout.Section>
+                    {loadingMarkup}
+                    {emptyStateMarkup}
+                </Layout.Section>
+            </Layout>
+        </Page>
+    );
 }
